6b875de03e0387356d999a14d31a28d4636d83c8
Cazeres Mathieu <mathieucazeres@gmail.com>
2023-01-21 15:58:56 +0100

    ajoute des citations d'intro et conclusion et structure des diplomes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState  } from "react"
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { ScrollToPlugin } from "gsap/ScrollToPlugin"
@@ -7,7 +7,37 @@ import moi from "./moi.png"
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 
+
+
+
+
+
+
 function App() {
+
+  const [formations, setFormations] = useState([
+    {
+      title: "Formation 1",
+      period: "Du 01/01/2021 au 01/01/2022",
+      location: "Paris, France",
+      mention: "Bien"
+    },
+    {
+      title: "Formation 2",
+      period: "Du 01/01/2022 au 01/01/2023",
+      location: "Lyon, France",
+      mention: "Très bien"
+    },
+    {
+      title: "Formation 3",
+      period: "Du 01/01/2023 au 01/01/2024",
+      location: "New York, USA",
+      mention: "Excellent"
+    }
+  ]);
+
+  let intro = "Je suis actuellement étudiant en  Master d'informatique et passionné par le développement web et JavaScript, je souhaite développer mes connaissances dans l’intégration de technologies web pour créer des applications modernes et performantes. Je suis à la recherche d'une alternance pour ma dernière année de master afin de mettre en pratique mes connaissances et de continuer à développer mes compétences dans ce domaine passionnant.";
+  let conclusion = "Ayant acquis une solide expérience en résolution de problèmes techniques grâce à mes projets de fin d'études et à mes expériences professionnelles. Je suis également habitué à apprendre rapidement de nouvelles technologies et de nouveaux outils, afin de me tenir à jour dans le domaine en constante évolution. Enfin, je suis efficace dans le travail d’équipe, j'ai déjà travaillé en collaboration avec d'autres membres pour atteindre des objectifs communs. Je suis convaincu que ces compétences font de moi un atout pour tout projet de développement web et je suis impatient de continuer à développer ces compétences.";
 
   const titleRef = useRef()
 
@@ -15,6 +45,7 @@ function App() {
     gsap.timeline({
       onComplete: function () {
         console.log('animation terminée')
+
       }
     })
       .fromTo(".letter",
@@ -26,7 +57,7 @@ function App() {
         x: 0,
         opacity: 1,
         stagger: 0.33,
-        delay: 0.7
+        delay: 0.4
       }      
     )
     .to(".title", {
@@ -35,30 +66,9 @@ function App() {
     })
     .to(".letter", {
       x: 0,
-      delay: 1,
+      delay: 0.5,
       duration: 2
     })
-  }
-
-  const slideInTop = (elem, delay, duration) => {
-    gsap.fromTo(
-      elem,
-      {
-        opacity: 0,
-        y: -200,
-      },
-      {
-        opacity: 1,
-        y: 0,
-        delay: delay || 0.4,
-        duration: duration || 0.6,
-        scrollTrigger: {
-          trigger: elem,
-          start: "top center",
-          end: "bottom center"
-        }
-      }
-    )
   }
 
   const slideInLeft = (elem, delay, duration) => {
@@ -66,7 +76,7 @@ function App() {
       elem,
       {
         opacity: 0,
-        x: -200,
+        x: -400,
       },
       {
         opacity: 1,
@@ -81,6 +91,28 @@ function App() {
       }
     )
   }
+
+  const slideInRight = (elem, delay, duration) => {
+    gsap.fromTo(
+      elem,
+      {
+        opacity: 0,
+        x: 400,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        delay: delay || 0.4,
+        duration: duration || 0.6,
+        scrollTrigger: {
+          trigger: elem,
+          start: "top center",
+          end: "bottom center"
+        }
+      }
+    )
+  }
+
 
   const onEnter = ({ currentTarget }) => {
     gsap.to(currentTarget, { backgroundColor: "#000", color: "#FFF" })
@@ -94,18 +126,22 @@ function App() {
     onLoad();
   }, [])
   useEffect(() => {
-    slideInTop("#box1");
+    slideInLeft("#intro");
   }, [])
   useEffect(() => {
-    slideInTop("#box2");
+    slideInRight("#box1");
   }, [])
   useEffect(() => {
-    slideInLeft("#box3");
+    slideInLeft("#box2");
   }, [])
   useEffect(() => {
-    slideInLeft("#box4");
+    slideInRight("#box3");
   }, [])
-  
+  useEffect(() => {
+    slideInLeft("#conclusion");
+  }, [])
+
+
   return (
     <div className="App">
       <div className="divtitle">
@@ -118,7 +154,7 @@ function App() {
         <span className="letter">R</span>
         <span className="letter">E</span>
         <span className="letter">S</span>
-        ㅤㅤㅤ
+        ㅤ
         <span className="letter">M</span>
         <span className="letter">A</span>
         <span className="letter">T</span>
@@ -128,10 +164,26 @@ function App() {
         <span className="letter">U</span>
       </h1>
       </div>
+
       
       <section id="nextSection">
+
+      
+
+      <div id="intro" className="box" onMouseEnter={onEnter} onMouseLeave={onLeave}>
+          {intro}
+        </div>
+
         <div id="box1" className="box" onMouseEnter={onEnter} onMouseLeave={onLeave}>
-          Diplomes
+          Formations
+            {formations.map((formation, index) => (
+              <div className="diploma-container" key={index}>
+                <div className="diploma-title">{formation.title}</div>
+                <div className="diploma-period">Période : {formation.period}</div>
+                <div className="diploma-location">Lieu : {formation.location}</div>
+                <div className="diploma-mention">Mention : {formation.mention}</div>
+              </div>
+            ))}
         </div>
         <div id="box2" className="box"  onMouseEnter={onEnter} onMouseLeave={onLeave}>
           Compétences
@@ -139,14 +191,19 @@ function App() {
         <div id="box3" className="box"  onMouseEnter={onEnter} onMouseLeave={onLeave}>
           Expériences
         </div>
-        <div id="box4" className="box"  onMouseEnter={onEnter} onMouseLeave={onLeave}>
-          Centres d'intérêt
+        <div id="conclusion" className="box"  onMouseEnter={onEnter} onMouseLeave={onLeave}>
+            {conclusion}
         </div>
       </section>
 
       <section id="lastSection">
         <div id="box5" className="box" onMouseEnter={onEnter} onMouseLeave={onLeave}>
           Coordonnées
+          <p className="coord">mathieucazeres@gmail.com</p>
+          <p className="coord">0783048656</p>
+          <p className="coord">34090 Montpellier France</p>
+
+
         </div>
       </section>
     </div>
